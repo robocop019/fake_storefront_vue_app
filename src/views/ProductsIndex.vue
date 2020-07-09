343ed7572569d387b6a950942265b10eba10b837
robocop019 <chrisbowen@comcast.net>
2020-07-09 16:37:57 -0500

    minor text changes

--- src/views/ProductsIndex.vue
+++ src/views/ProductsIndex.vue
@@ -4,15 +4,29 @@
 
       <h1>All Products</h1>
 
+      <div>
+        <button class="btn btn-dark btn-outline-info" v-on:click="setSortAttribute('name')">
+          Sort by Name <span v-if="sortAttribute === 'name'"> {{sortArrow}} </span>
+        </button>
+
+        <button class="btn btn-dark btn-outline-info" v-on:click="setSortAttribute('price')">
+          Sort by Price <span v-if="sortAttribute === 'price'"> {{sortArrow}} </span>
+        </button>
+      </div>
+
       <div class="row">
 
-        <div v-for="product in orderBy(filterBy(products, $parent.nameFilter, 'name'), sortAttribute)">
+        <div v-for="product in orderBy(filterBy(products, $parent.nameFilter, 'name'), sortAttribute, sortAscending)">
 
           <div class="col-md-4 offset-2">
             <div class="card text-white bg-dark mb-5">
               <img v-if='product.images[0]' :src="product.images[0].url" :alt="product.name" class="card-img-top">
               <div class="card-body">
-                <h3 class="card-text"><router-link :to="'/products/' + product.id"> {{ product.name }} </router-link></h3>
+                <h3 class="card-text">
+                  <router-link :to="'/products/' + product.id"> 
+                    {{ product.name }} - {{product.formatted.price}} 
+                  </router-link>
+                </h3>
               </div>
             </div>
           </div>
@@ -21,7 +35,7 @@
 
       </div> <!-- end .row -->
     </div> <!-- end .container -->
-  </div> <!-- end .home -->
+  </div> <!-- end .products-index -->
 </template>
 
 <style>
@@ -36,7 +50,9 @@
     data: function() {
       return {
         products: [],
-        sortAttribute: 'name'
+        sortAttribute: 'name',
+        sortAscending: 1,
+        sortArrow: '⬆️'
       };
     },
     created: function() {
@@ -44,7 +60,23 @@
         this.products = response.data;
       });
     },
-    methods: {},
+    methods: {
+      setSortAttribute: function(inputAttribute) {
+
+        if (this.sortAttribute === inputAttribute) {
+          this.sortAscending *= -1;
+          if (this.sortArrow === '⬆️') {
+            this.sortArrow = '⬇️';
+          } else {
+            this.sortArrow = '⬆️';
+          }
+        } else {
+          this.sortAscending = 1;
+          this.sortArrow = '⬆️';
+        }
+        this.sortAttribute = inputAttribute;
+      }
+    },
     mixins: [Vue2Filters.mixin]
   };
 </script>
